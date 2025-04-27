d877e350bf6ebff39785d532123c5d2b7a42a921
Tsotne Bukiya <tsotnebukiya@gmail.com>
2025-04-27 15:20:54 +0400

    WidgetInit

--- app.config.ts
+++ app.config.ts
@@ -41,6 +41,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     'expo-router',
     'expo-localization',
     'expo-apple-authentication',
+    '@bacons/apple-targets',
     [
       'expo-notifications',
       {
